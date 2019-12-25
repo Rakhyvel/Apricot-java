3a82415540517b2462a2fc58b5969c4cf42c2196
Joseph Shimel <josephmshimel@gmail.com>
2019-12-25 15:37:27 -0700

    Moved overlayImage to Image class

--- src/com/josephs_projects/apricotLibrary/graphics/Image.java
+++ src/com/josephs_projects/apricotLibrary/graphics/Image.java
@@ -31,4 +31,44 @@ public class Image {
 		}
 		return image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
 	}
+	
+	/**
+	 * Converts a gray image to a colored image
+	 * 
+	 * @param color Color to overlay
+	 * @param img Gray image
+	 * @return An integer array that represents the new image
+	 */
+	public int[] overlayBlend(int color, int[] img) {
+		int r, g, b, a;
+		float screen, alpha;
+		int[] img2 = new int[img.length];
+		for (int i = 0; i < img.length; i++) {
+			alpha = ((img[i] >> 24 & 255) / 255.0f);
+			if (alpha < 0.9f)
+				continue;
+			
+			// Finding and splitting starting colors
+			screen = (img[i] & 255) / 255.0f;
+			a = ((color >> 24) & 255);
+			r = ((color >> 16) & 255);
+			g = ((color >> 8) & 255);
+			b = (color & 255);
+
+			// Setting new colors
+			if (screen <= 0.5f) {
+				screen *= 2;
+				r = (int) (r * screen);
+				g = (int) (g * screen);
+				b = (int) (b * screen);
+			} else {
+				r = (int) (255 - 2 * (255 - r) * (1 - screen));
+				g = (int) (255 - 2 * (255 - g) * (1 - screen));
+				b = (int) (255 - 2 * (255 - b) * (1 - screen));
+			}
+			// Recombining colors
+			img2[i] = (a << 24) | (int) (r * alpha) << 16 | (int) (g * alpha) << 8 | (int) (b * alpha);
+		}
+		return img2;
+	} // overlayBlend()
 }
--- src/com/josephs_projects/apricotLibrary/graphics/Render.java
+++ src/com/josephs_projects/apricotLibrary/graphics/Render.java
@@ -8,6 +8,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import com.josephs_projects.apricotLibrary.Apricot;
+
 /**
  * Render is used to quickly draw graphics to the screen and is faster than
  * using AWT's Graphics class
@@ -238,51 +240,11 @@ public class Render {
 
 		for (int i = 0; i < text.length(); i++) {
 			letter = text.charAt(i);
-			letterImage = overlayBlend(color, font.getLetter(letter));
+			letterImage = Apricot.image.overlayBlend(color, font.getLetter(letter));
 			drawImage(x + carriage + font.getSize() / 2, y, font.getSize(), letterImage, 1, 0);
 			carriage += font.getKern(letter);
 		}
 	}
-
-	/**
-	 * Converts a gray image to a colored image
-	 * 
-	 * @param color Color to overlay
-	 * @param img Gray image
-	 * @return An integer array that represents the new image
-	 */
-	public int[] overlayBlend(int color, int[] img) {
-		int r, g, b, a;
-		float screen, alpha;
-		int[] img2 = new int[img.length];
-		for (int i = 0; i < img.length; i++) {
-			alpha = ((img[i] >> 24 & 255) / 255.0f);
-			if (alpha < 0.9f)
-				continue;
-			
-			// Finding and splitting starting colors
-			screen = (img[i] & 255) / 255.0f;
-			a = ((color >> 24) & 255);
-			r = ((color >> 16) & 255);
-			g = ((color >> 8) & 255);
-			b = (color & 255);
-
-			// Setting new colors
-			if (screen <= 0.5f) {
-				screen *= 2;
-				r = (int) (r * screen);
-				g = (int) (g * screen);
-				b = (int) (b * screen);
-			} else {
-				r = (int) (255 - 2 * (255 - r) * (1 - screen));
-				g = (int) (255 - 2 * (255 - g) * (1 - screen));
-				b = (int) (255 - 2 * (255 - b) * (1 - screen));
-			}
-			// Recombining colors
-			img2[i] = (a << 24) | (int) (r * alpha) << 16 | (int) (g * alpha) << 8 | (int) (b * alpha);
-		}
-		return img2;
-	} // overlayBlend()
 
 	/**
 	 * Returns the 32bit color equivalent
