5d5a205ac0959ef2cc4ffdd86bf9bd0d57f4830d
Joseph Shimel <josephmshimel@gmail.com>
2020-12-04 20:54:20 -0700

    Add net class that opens default web browser
    Needed it for tangerine to report bugs. Will probably need it other
    places too.

---
+++ src/com/josephs_projects/apricotLibrary/net/Net.java
@@ -0,0 +1,29 @@
+package com.josephs_projects.apricotLibrary.net;
+
+import java.awt.Desktop;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Net {
+	public static boolean openWebpage(java.net.URI uri) {
+	    Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+	    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+	        try {
+	            desktop.browse(uri);
+	            return true;
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return false;
+	}
+	
+	public static boolean openWebpage(URL url) {
+	    try {
+	        return openWebpage(url.toURI());
+	    } catch (URISyntaxException e) {
+	        e.printStackTrace();
+	    }
+	    return false;
+	}
+}
